68ff851d8c9c00987b6093f4a2954de708718e04
Armin Ronacher <armin.ronacher@active-4.com>
2022-08-03 19:15:47 +0200

    rustfmt

--- examples/custom.rs
+++ examples/custom.rs
@@ -1,5 +1,8 @@
 use similar_asserts::SimpleDiff;
 
 fn main() {
-    panic!("Not equal\n\n{}", SimpleDiff::from_str("a\nb\n", "b\nb\n", "left", "right"));
+    panic!(
+        "Not equal\n\n{}",
+        SimpleDiff::from_str("a\nb\n", "b\nb\n", "left", "right")
+    );
 }
